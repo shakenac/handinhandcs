6eae43e943851d6b4e598baedb4437516336bc1e
Shakena <shakenacme@gmail.com>
2020-12-25 00:21:17 -0500

    Commented on script.js the steps for nav menu toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+/*
+   MOBILE NAVIGATION TOGGLE:
 
+   1. Hide original Navigation
+   2. User clicks on hamburger direction
+         2a. Display Navigation
+            - Navigation displays as column
+            -
+   3. User clicks the menu icon &
+      User clicks page (off of the navigation)
+         3a. Hide Navigation
+
+*/
