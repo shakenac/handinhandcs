1a0221070a894e18a4cd540a9d60c0ed6426aa8e
Shakena <shakenacme@gmail.com>
2020-12-25 19:59:09 -0500

    Began javascript for mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
    MOBILE NAVIGATION TOGGLE:
 
-   1. Hide original Navigation
+   1. Hide original Navigation [DONE]
    2. User clicks on arrow icon
          2a. Display (toggle on) Navigation
             - Navigation displays as column
@@ -13,9 +13,21 @@
 */
 
 // Target the .mobile-nav-icon
-const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon');
-
-mobileNavIcon.addEventListener('click', () => {
-   // if user clicks mobileNavIcon
-   // .navbar = display:flex
-})
+// const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon');
+//
+// let toggleOn = function() {
+//
+//    mobileNavIcon.addEventListener('click', () => {
+//       // if user clicks mobileNavIcon
+//
+//       // nav will display
+//       .navbar nav ()
+//    })
+// }
+//
+// if
+//    mobileNavIcon.addEventListener('click', () => {
+//       // if user clicks mobileNavIcon
+//       if ()
+//       // .navbar = display:flex
+//    })
