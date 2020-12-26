2e05562b0584e906b4bc22f6d9acba0424631902
Shakena <shakenacme@gmail.com>
2020-12-25 19:22:18 -0500

    Hide mobile nav icon when in desktop mode

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@
    MOBILE NAVIGATION TOGGLE:
 
    1. Hide original Navigation
-   2. User clicks on hamburger direction
-         2a. Display Navigation
+   2. User clicks on arrow icon
+         2a. Display (toggle on) Navigation
             - Navigation displays as column
-            -
-   3. User clicks the menu icon &
-      User clicks page (off of the navigation)
-         3a. Hide Navigation
+            - Arrow transform: rotates 180 degrees when clicked on
+   3. User clicks the menu icon; User clicks outside of menu:
+         3a. Hide (toggle off) Navigation
+            - Arrow transform: rotates back
 
 */
+
+// Target the .mobile-nav-icon
+const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon');
+
+mobileNavIcon.addEventListener('click', () => {
+   // if user clicks mobileNavIcon
+   // .navbar = display:flex
+})
