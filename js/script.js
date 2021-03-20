f5bae5df91283734dc0cb72a09e727adb10a640f
Shakena <shakenacme@gmail.com>
2021-03-19 23:42:27 -0400

    Added click event to open and close mobile nav

--- js/script.js
+++ js/script.js
@@ -1,39 +1,28 @@
-const openMenu = document.getElementsByClassName('mobile-nav-icon')[0];
-const mobileNavOverlay = document.getElementsByClassName('mobile-nav-overlay')[0];
-const closeMenu = document.getElementsByClassName('mobile-nav-icon-open')[0];
-
-
 /*
    MOBILE NAVIGATION TOGGLE:
 
    1. Hide original Navigation [DONE]
    2. User clicks on arrow icon
-         2a. Display (toggle on) .mobile-nav-icon-open
+         2a. Display (turn on) .mobile-nav-icon-open
             - add .active class on mobile-nav-icon-open
 
    3. User clicks the menu icon; User clicks outside of menu:
-         3a. Hide (turn) off) .mobile-nav-icon-open
+         3a. Hide (turn off) .mobile-nav-icon-open
             - remove .active class on mobile-nav-icon-open
 
 */
 
+const openMenu = document.getElementsByClassName('mobile-nav-icon')[0];
+const mobileNavOverlay = document.getElementsByClassName('mobile-nav-overlay')[0];
+const closeMenu = document.getElementsByClassName('mobile-nav-icon-open')[0];
 
-// Target the .mobile-nav-icon
-// const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon');
-//
-// let toggleOn = function() {
-//
-//    mobileNavIcon.addEventListener('click', () => {
-//       // if user clicks mobileNavIcon
-//
-//       // nav will display
-//       .navbar nav ()
-//    })
-// }
-//
-// if
-//    mobileNavIcon.addEventListener('click', () => {
-//       // if user clicks mobileNavIcon
-//       if ()
-//       // .navbar = display:flex
-//    })
+
+// turn on mobile nav
+openMenu.addEventListener('click', () => {
+   mobileNavOverlay.classList.add('active');
+})
+
+// turn off mobile nav
+closeMenu.addEventListener('click', () => {
+   mobileNavOverlay.classList.remove('active');
+})
