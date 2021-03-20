f4db20eefe1345f54dcafa0dd5f1371396cd968e
Shakena <shakenacme@gmail.com>
2021-03-19 23:22:03 -0400

    (JavaScript file) Created variables for mobile menu and hamburger icons

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
+const openMenu = document.getElementsByClassName('mobile-nav-icon')[0];
+const mobileNavOverlay = document.getElementsByClassName('mobile-nav-overlay')[0];
+const closeMenu = document.getElementsByClassName('mobile-nav-icon-open')[0];
+
+
 /*
    MOBILE NAVIGATION TOGGLE:
 
    1. Hide original Navigation [DONE]
    2. User clicks on arrow icon
-         2a. Display (toggle on) Navigation
-            - Navigation displays as column
-            - Arrow transform: rotates 180 degrees when clicked on
+         2a. Display (toggle on) .mobile-nav-icon-open
+            - add .active class on mobile-nav-icon-open
+
    3. User clicks the menu icon; User clicks outside of menu:
-         3a. Hide (toggle off) Navigation
-            - Arrow transform: rotates back
+         3a. Hide (turn) off) .mobile-nav-icon-open
+            - remove .active class on mobile-nav-icon-open
 
 */
+
 
 // Target the .mobile-nav-icon
 // const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon');
