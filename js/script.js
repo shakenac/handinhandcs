dfb6318834fec1ef30a3abcd95e72f2bf39e8a5b
Shakena <shakenacme@gmail.com>
2021-03-20 01:13:54 -0400

    Removed mobile-nav-icon-open. Rotate mobile-nav-icon when clicked

--- js/script.js
+++ js/script.js
@@ -1,28 +1,46 @@
+const mobileIcon = document.getElementsByClassName('mobile-nav-icon')[0];
+const mobileNavOverlay = document.getElementsByClassName('mobile-nav-overlay')[0];
+
 /*
    MOBILE NAVIGATION TOGGLE:
-
    1. Hide original Navigation [DONE]
    2. User clicks on arrow icon
-         2a. Display (turn on) .mobile-nav-icon-open
-            - add .active class on mobile-nav-icon-open
+         2a. Display (toggle on) .mobile-nav-icon-open
+            - toggle on .active class on mobile-nav-icon-open
+            - Arrow transform: rotates 180 degrees when clicked on
 
    3. User clicks the menu icon; User clicks outside of menu:
-         3a. Hide (turn off) .mobile-nav-icon-open
-            - remove .active class on mobile-nav-icon-open
+         3a. Hide (toggle off) .mobile-nav-icon-open
+            - toggle off .active class on mobile-nav-icon-open
+            - Arrow transform: rotates back
 
 */
 
-const openMenu = document.getElementsByClassName('mobile-nav-icon')[0];
-const mobileNavOverlay = document.getElementsByClassName('mobile-nav-overlay')[0];
-const closeMenu = document.getElementsByClassName('mobile-nav-icon-open')[0];
 
-
-// turn on mobile nav
-openMenu.addEventListener('click', () => {
-   mobileNavOverlay.classList.add('active');
+// Arrow rotates when clicked
+mobileIcon.addEventListener('click', () => {
+   mobileIcon.classList.add('change');
 })
 
-// turn off mobile nav
-closeMenu.addEventListener('click', () => {
-   mobileNavOverlay.classList.remove('active');
-})
+
+
+
+// Target the .mobile-nav-icon
+// const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon');
+//
+// let toggleOn = function() {
+//
+//    mobileNavIcon.addEventListener('click', () => {
+//       // if user clicks mobileNavIcon
+//
+//       // nav will display
+//       .navbar nav ()
+//    })
+// }
+//
+// if
+//    mobileNavIcon.addEventListener('click', () => {
+//       // if user clicks mobileNavIcon
+//       if ()
+//       // .navbar = display:flex
+//    })
